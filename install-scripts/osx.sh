bdc2cb686c2b100fff7a1b22e5635a96b223fceb
Alex Reardon <areardon@atlassian.com>
2019-07-02 20:39:11 +1000

    more osx

--- install-scripts/osx.sh
+++ install-scripts/osx.sh
@@ -11,7 +11,7 @@ defaults write NSGlobalDomain KeyRepeat -int 2
 # System Preferences > Keyboard >
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
-echo "Dock 🛳"
+echo "System preferences"
 
 # System Preferences > Dock > Size:
 defaults write com.apple.dock tilesize -int 36
@@ -25,6 +25,26 @@ defaults write com.apple.dock largesize -int 54
 # System Preferences > Dock > Automatically hide and show the Dock:
 defaults write com.apple.dock autohide -bool false
 
+# System Preferences > Dock > Minimize windows into application icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# System Preferences > General > Click in the scrollbar to: Jump to the spot that's clicked
+defaults write -globalDomain "AppleScrollerPagingBehavior" -bool true
+
+# System Preferences > Dock > Show indicators for open applications
+defaults write com.apple.dock show-process-indicators -bool true
+
+echo "Finder"
+
+# Finder > Preferences > Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder > Preferences > Show wraning before changing an extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Finder > View > Show Path Bar
+defaults write com.apple.finder ShowPathbar -bool true
+
 echo "Mission Control"
 
 # System Preferences > Mission Controll > Automatically rearrange Spaces based on most recent use
@@ -34,6 +54,11 @@ echo "Misc 📦"
 
 # Completely Disable Dashboard
 defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Kill affected apps
+for app in "Dock" "Finder"; do
+  killall "${app}" > /dev/null 2>&1
+done
 
 echo "Mac configuration finished 💻"
 echo "Note that some of these changes require a logout/restart to take effect."
